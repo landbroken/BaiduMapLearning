958ed33dea712d6c91a3e45f2b27729b2c9b31b9
landbroken <445299258@qq.com>
2018-09-30 21:40:11 +0800

    Initialize

---
+++ MyMap/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace MyMap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.marker = new System.Windows.Forms.Button();
+            this.removeListen = new System.Windows.Forms.Button();
+            this.clearMarker = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(913, 502);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 480);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(913, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(131, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "测距";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnOpenDistance_Click);
+            // 
+            // marker
+            // 
+            this.marker.Location = new System.Drawing.Point(41, 12);
+            this.marker.Name = "marker";
+            this.marker.Size = new System.Drawing.Size(75, 23);
+            this.marker.TabIndex = 2;
+            this.marker.Text = "标记";
+            this.marker.UseVisualStyleBackColor = true;
+            this.marker.Click += new System.EventHandler(this.marker_Click);
+            // 
+            // removeListen
+            // 
+            this.removeListen.Location = new System.Drawing.Point(456, 12);
+            this.removeListen.Name = "removeListen";
+            this.removeListen.Size = new System.Drawing.Size(75, 23);
+            this.removeListen.TabIndex = 3;
+            this.removeListen.Text = "默认";
+            this.removeListen.UseVisualStyleBackColor = true;
+            this.removeListen.Click += new System.EventHandler(this.removeListen_Click);
+            // 
+            // clearMarker
+            // 
+            this.clearMarker.Location = new System.Drawing.Point(244, 12);
+            this.clearMarker.Name = "clearMarker";
+            this.clearMarker.Size = new System.Drawing.Size(75, 23);
+            this.clearMarker.TabIndex = 4;
+            this.clearMarker.Text = "清空";
+            this.clearMarker.UseVisualStyleBackColor = true;
+            this.clearMarker.Click += new System.EventHandler(this.clearMarker_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 502);
+            this.Controls.Add(this.clearMarker);
+            this.Controls.Add(this.removeListen);
+            this.Controls.Add(this.marker);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button marker;
+        private System.Windows.Forms.Button removeListen;
+        private System.Windows.Forms.Button clearMarker;
+    }
+}
+
---
+++ MyMap/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Permissions;
+using System.Collections;
+
+namespace MyMap
+{
+    //固定格式
+    [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]  
+    [System.Runtime.InteropServices.ComVisibleAttribute(true)]   
+
+    public partial class Form1 : Form
+    {
+        //初始化控件，固定格式
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //载入百度地图
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             string str_url = Application.StartupPath + "\\IndexMap.html";  
+             Uri url = new Uri(str_url);
+             webBrowser1.Url = url;
+             webBrowser1.ObjectForScripting = this;
+             timer1.Enabled = true;
+        }
+
+        //定时刷新
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                //获得当前鼠标所在的经纬度，然后显示在框体左下角
+                string tag_lng = webBrowser1.Document.GetElementById("mouselng").InnerText;  
+                string tag_lat = webBrowser1.Document.GetElementById("mouselat").InnerText;  
+                double dou_lng, dou_lat;  
+                if (double.TryParse(tag_lng, out dou_lng) && double.TryParse(tag_lat, out dou_lat))  
+                {
+                    this.toolStripStatusLabel1.Text ="当前坐标："+ dou_lng.ToString("F5") + "," + dou_lat.ToString("F5");  
+                }  
+            }  
+            catch (Exception ee)  
+            { 
+                //MessageBox.Show(ee.Message); 
+            }
+        }
+
+        //数字转字符串 
+        public string setWhichCar(int num)
+        {
+            return num.ToString();
+        }
+
+        //开启测距工具按钮,点击测距按钮后触发 
+        private void btnOpenDistance_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Document.InvokeScript("openGetDistance");
+        }
+        
+        //右键添加标记到图中,点击标记按钮后触发
+        private void marker_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Document.InvokeScript("PutInMarker");
+            MessageBox.Show("请按右键添加标注！");
+        }
+
+        //清除监听事件，点击默认按钮后触发
+        private void removeListen_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Document.InvokeScript("CloseListener");  
+        }
+
+        //清除地图上的标记，点击清除按钮触发
+        private void clearMarker_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Document.InvokeScript("ClearAllMarkers");  
+        } 
+
+    }
+}
